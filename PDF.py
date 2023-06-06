9613c29cdbe9a068577d1d9262088062f2c9a172
Emanuel <emanuel.morille@proton.me>
2023-06-06 19:29:13 +0200

    refactor: simplify code for ocr

--- PDF.py
+++ PDF.py
@@ -4,8 +4,8 @@ import numpy as np
 import numexpr as ne
 from PIL import Image, ImageOps
 from pdf2image import convert_from_path
+
 from TextZone import Line, LETTER_THRESHOLD
-from utils import transpose
 
 
 class Page:
@@ -20,99 +20,104 @@ class Page:
 
     def __init__(self, image: Image.Image):
         self.image = image
-        self.array = np.array(image)
         self.width = image.width
         self.height = image.height
+        self.array: np.ndarray = np.array(image)
 
     @staticmethod
     def from_array(array: np.ndarray) -> Page:
-        return Page(Image.fromarray(array))
+        return Page(Image.fromarray(array.astype(np.uint8)))
 
-    def show(self):
-        self.image.show()
-
-    def grayscaled(self) -> Page:
-        return Page(ImageOps.grayscale(self.image))
-    def grayscaledTHRESHOLD(self):
-        tr = LETTER_THRESHOLD
-        arr = self.grayscaled().array
-        return Page.from_array(ne.evaluate("where(arr <= tr, 0, 255)"))
-    def rgb_array(self) -> np.ndarray:
-        if len(self.array.shape) == 3:
+    @property
+    def array_rgb(self) -> np.ndarray:
+        if self.array.ndim == 3:
             return self.array.copy()
         else:
             return np.dstack((self.array, self.array, self.array))
 
-    def set_lines(self):
-        self.lines = []
+    def grayscaled(self) -> Page:
+        return Page(ImageOps.grayscale(self.image))
 
+    def thresholded(self) -> Page:
+        threshold: int = LETTER_THRESHOLD
+        array = self.grayscaled().array
+        return Page.from_array(ne.evaluate("where(arrat <= threshold, 0, 255)"))
+
+    def set_lines(self):
+        if self.lines is not None:
+            return
+
+        self.lines = []
         start: int = -1
         inLine: bool = False
         for i, line in enumerate(self.grayscaled().array):
             if inLine and sum(line) / self.width == 255:
                 self.lines.append(Line(start, i))
                 inLine = False
-            elif not (inLine) and sum(line) / self.width != 255:
+            elif not inLine and sum(line) / self.width != 255:
                 start = i
                 inLine = True
 
-    def show_lines(self):
-        if self.lines == None:
-            self.set_lines()
-        
-        parsed = self.rgb_array()
+    def set_words(self):
+        self.set_lines()
+
+        array = self.grayscaled().array
         for line in self.lines:
-            parsed[line.top] = [[0, 255, 0] for _ in range(self.width)]
-            parsed[line.bottom] = [[255, 0, 0] for _ in range(self.width)]
+            line.set_words(array[line.top : line.bottom])
+
+    def set_letters(self):
+        self.set_words()
+
+        array = self.grayscaled().array
+        for line in self.lines:
+            for word in line.words:
+                word.set_letters(array[line.top : line.bottom])
+
+    def show(self):
+        self.image.show()
+
+    def show_lines(self):
+        self.set_lines()
+
+        parsed = self.array_rgb
+        for line in self.lines:
+            parsed[line.top - 1] = [0, 255, 0]
+            parsed[line.bottom] = [255, 0, 0]
+
         return Page.from_array(np.array(parsed)).show()
 
     def show_words(self):
-        if self.lines == None:
-            self.set_lines()
-        for line in self.lines:
-            line.set_words(self.grayscaled().array)
-        
-        parsed = self.rgb_array()
+        self.set_words()
+
+        parsed = self.array_rgb
         for line in self.lines:
             for word in line.words:
-                parsed[line.top][word.left : word.right] = [0, 255, 0]
-                parsed[line.bottom][word.left : word.right] = [255, 0, 0]
+                parsed[line.top - 1, word.left : word.right + 1] = [0, 255, 0]
+                parsed[line.bottom, word.left : word.right + 1] = [255, 0, 0]
+
         return Page.from_array(np.array(parsed)).show()
 
     def show_letters(self):
-        if self.lines == None:
-            self.set_lines()
+        self.set_letters()
 
-        for line in self.lines:
-            line.set_letters(self.grayscaledTHRESHOLD().array)
-        # self.lines[0].set_letters(self.grayscaled().array)
-        # self.lines = [self.lines[0]]
-        
-        parsed = self.rgb_array()
+        parsed = self.array_rgb
         for line in self.lines:
             for word in line.words:
-                parsed[line.top - 1, word.left: word.right] = [0, 255, 0]
-                parsed[line.bottom, word.left: word.right] = [255, 0, 0]
+                parsed[line.top - 1, word.left : word.right + 1] = [0, 255, 0]
+                parsed[line.bottom, word.left : word.right + 1] = [255, 0, 0]
 
                 alternate: bool = False
-                temp = -1
                 for letter in word.letters:
-                    if letter.left == temp:
-                        parsed[line.bottom + 5: line.bottom + 7, letter.left: letter.right] = [255, 0, 0]
-                    temp = letter.right
+                    parsed[
+                        line.bottom : line.bottom + 3,
+                        letter.left : letter.right,
+                        2,
+                    ] -= (
+                        100 if alternate else 200
+                    )
+                    alternate = not alternate
 
-                    parsed[line.bottom: line.bottom + 5, letter.left: letter.right, 2] -= 25 if alternate else 75
-                    # parsed[line.top + letter.pixels[0][1], letter.pixels[0][0]] = [255, 0, 0]
-                    # for pixel in letter.pixels[1:]:
-                    #     parsed[line.top + pixel[1], pixel[0]] = [0, 0, 255] if alternate else [0, 0, 150]
-                    alternate = not(alternate)
-        return Page.from_array(parsed.astype(np.uint8)).show()
-    def show_columns(self):
-        parsed = transpose(self.array)
-        for j, column in enumerate(transpose(self.grayscaled().array)):
-            if sum(column) / self.height == 255:
-                parsed[j] = [[255, 0, 0] for _ in range(self.height)]
-        return Page.from_array(np.array(transpose(parsed))).show()
+        return Page.from_array(parsed).show()
 
 
 class PDF:
@@ -126,6 +131,6 @@ class PDF:
         if ext == "pdf":
             self.pages: list[Page] = [Page(image) for image in convert_from_path(path)]
         elif ext == "png":
-            self.pages: list[Page] = [Page(Image.open(path))]
+            self.pages: list[Page] = [Page(Image.open(path).convert("RGB"))]
         else:
             raise TypeError("Wrong extension")
--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 from PDF import PDF
 
 test_1 = PDF("test/test_1_toLatex.png")
-# test_1.pages[0] = test_1.pages[0].grayscaledTHRESHOLD()
 test_1.pages[0].show_letters()
-# test_1.pages[0].show_columns()
-# test_1.pages[0].show()
