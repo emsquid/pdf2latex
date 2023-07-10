9561f96394998ed22887fb9f754b39e40271e4af
Emanuel <emanuel.morille@proton.me>
2023-07-10 16:16:14 +0200

    chore: remove python testings

--- python/main.py
+++
@@ -1,9 +0,0 @@
-from PDF import PDF
-from RandomPDF import randomPDF
-import os
-
-if __name__ == "__main__":
-    # randomPDF("test/test_2_toLatex.pdf")
-    # test = PDF(os.path.dirname(os.path.realpath(__file__)) + "/temp/random.pdf")
-    test = PDF("test/test_1_toLatex.pdf")
-    test.pages[0].show_letters()
